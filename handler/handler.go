eda41af5390326395d4045c91eaf3c65a10f7c20
atsuk0710 <atsukodan10@gmail.com>
2021-06-07 16:18:50 +0800

    读取、返回http请求

---
+++ handler/handler.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"go-api-demo/pkg/errno"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func SendResponse(c *gin.Context, err error, data interface{})  {
+	code, message := errno.DecodeErr(err)
+
+	// 固定返回格式
+	c.JSON(http.StatusOK, Response{
+		Code: code,
+		Message: message,
+		Data: data,
+	})
+}
--- handler/user/create.go
+++ handler/user/create.go
@@ -2,8 +2,8 @@ package user
 
 import (
 	"fmt"
+	. "go-api-demo/handler"
 	"go-api-demo/pkg/errno"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
@@ -11,21 +11,29 @@ import (
 
 // Create creates a new user account.
 func Create(c *gin.Context) {
-	var r struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
+	var r CreateRequest
 
 	var err error
 	if err := c.Bind(&r); err != nil {
-		c.JSON(http.StatusOK, gin.H{"error": errno.ErrBind})
+		// c.JSON(http.StatusOK, gin.H{"error": errno.ErrBind})
+		SendResponse(c, errno.ErrBind, nil)
 		return
 	}
+
+	admin2 := c.Param("username")
+	log.Infof("URL username: %s", admin2)
+
+	desc := c.Query("desc")
+	log.Infof("URL key param desc: %s", desc)
+
+	contentType := c.GetHeader("Content-Type")
+	log.Infof("Header Content Type: %s", contentType)
 
 	log.Debugf("username is: [%s], password is [%s]", r.Username, r.Password)
 	if r.Username == "" {
 		err = errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")).Add("This is add message.")
-		log.Errorf(err, "Get an error")
+		SendResponse(c, err, nil)
+		return
 	}
 
 	if errno.IsErrUserNotFound(err) {
@@ -34,8 +42,14 @@ func Create(c *gin.Context) {
 
 	if r.Password == "" {
 		err = fmt.Errorf("password is empty")
+		SendResponse(c, err, nil)
 	}
 
-	code, message := errno.DecodeErr(err)
-	c.JSON(http.StatusOK, gin.H{"code": code, "message": message})
+	rsp := CreateResponse{
+		Username: r.Username,
+	}
+	SendResponse(c, nil, rsp)
+
+	// code, message := errno.DecodeErr(err)
+	// c.JSON(http.StatusOK, gin.H{"code": code, "message": message})
 }
